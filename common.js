0022b54d9f2c73d99519a95aaa1d95bc9bfb5761
devsequence <devsequencce@gmail.com>
2022-05-24 16:23:54 +0300

    commit

--- common.js
+++ common.js
@@ -1,4 +1,10 @@
 // console.log($('#grad_9'));
+// setTimeout(function() {
+//     $('.cls-2_1').addClass('opacity');
+// }, 3000);
 setTimeout(function() {
-    $('.cls-2_1').addClass('opacity');
-}, 3000);
+    $('.cls-6').addClass('active');
+}, 500);
+setTimeout(function() {
+    $('.cls-2').addClass('active');
+}, 1000);
