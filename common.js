3c8004cabc347d4f39a44af0462dcb80ee572325
devsequence <devsequencce@gmail.com>
2022-05-24 21:10:52 +0300

    test

--- common.js
+++ common.js
@@ -7,4 +7,39 @@ setTimeout(function() {
 }, 500);
 setTimeout(function() {
     $('.cls-2').addClass('active');
-}, 1000);
+}, 800);
+
+setTimeout(function() {
+    $('.logo').addClass('animation');
+}, 2800);
+setTimeout(function() {
+
+    $('.cls-2').addClass('opacity');
+    $('.cls-2_1').addClass('active');
+    $('.cls-9').addClass('active');
+    $('.cls-11').addClass('active');
+    $('.cls-6, .cls-3, .cls-5, .cls-4').addClass('opacity');
+    $('.cls-6_1, .cls-4_1, .cls-5_1').addClass('active');
+    $('.cls-7').addClass('animation');
+}, 3150);
+setTimeout(function() {
+    $('.logo').addClass('animationSecond');
+    $('.cls-6_1, .cls-4_1, .cls-5_1').removeClass('active');
+
+    $('.cls-7').removeClass('animation');
+    $('.cls-9').addClass('opacity');
+
+
+}, 5500);
+setTimeout(function() {
+    $('.cls-9_1').addClass('active');
+    $('.cls-2_2').addClass('active');
+    $('.cls-2_1').addClass('opacity');
+    $('.cls-6_1, .cls-4_1, .cls-5_1').addClass('active');
+    $('.cls-7').addClass('animation');
+    $('.cls-6').removeClass('opacity');
+
+
+    $('.cls-6_1').removeClass('active');
+}, 5800);
+
